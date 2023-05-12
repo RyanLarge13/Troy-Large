69d0ae5d464f7604587f81ab56266d6fe6e352fc
RyanLarge13 <ryanlarge13@gmail.com>
2023-05-11 23:55:17 -0700

    Tailwind Error

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -11,22 +11,40 @@ const Header = () => {
         <ul className="flex px-2 py-4 justify-around items-center bg-[#222] rounded-b-md shadow-md">
           <li>
             <Link href="/" legacyBehavior>
-              <a className={path === "/" && "text-amber-200"}>Home</a>
+              <a className={path === "/" ? "text-amber-200" : "text-white"}>
+                Home
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/gallery" legacyBehavior>
-              <a className={path === "/gallery" && "text-amber-200"}>Gallery</a>
+              <a
+                className={
+                  path === "/gallery" ? "text-amber-200" : "text-white"
+                }
+              >
+                Gallery
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/about" legacyBehavior>
-              <a className={path === "/about" && "text-amber-200"}>About</a>
+              <a
+                className={path === "/about" ? "text-amber-200" : "text-white"}
+              >
+                About
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/contact" legacyBehavior>
-              <a className={path === "/contact" && "text-amber-200"}>Contact</a>
+              <a
+                className={
+                  path === "/contact" ? "text-amber-200" : "text-white"
+                }
+              >
+                Contact
+              </a>
             </Link>
           </li>
         </ul>
