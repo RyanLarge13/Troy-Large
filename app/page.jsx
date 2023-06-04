fbc794ce0307dc6d3f64f33d1802db35ac6fbdbf
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-06-03 23:52:11 -0700

    Test

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,27 @@
+import mongoose from "mongoose";
+import { connectDB } from "@utils/connectDB.js";
+
 const getPaintings = async () => {
-	try {
-  const res = await fetch(`/api/paintings`);
-  return res.json();
-	} catch (err) {
-		console.log(err)
-	}
+  try {
+    await connectDB();
+    const paintings = mongoose.connection.collection("paintings");
+    const data = await paintings.find({}).toArray();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
 const Home = async () => {
-	const paintings = await getPaintings();
+  const paintings = await getPaintings();
   return (
     <section className="mt-20 text-white">
-      {
-        paintings.map((painting) => (
-          <div key={painting.id}>
-            <h2>{painting.Title}</h2>
-          </div>
-        
+      {paintings.map((painting) => (
+        <div key={painting._id}>
+          <h2>{painting.Title}</h2>
+        </div>
       ))}
     </section>
   );
