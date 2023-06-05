4b05f3ca412ebd43ebca7a5b3e11b726dcd99525
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-06-04 21:29:46 -0700

    Adding Metadata
    Adding a title, a favicon & device width settings.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,20 @@
 import "./globals.css";
+import Head from "next/head";
 import Header from "@components/Header";
 import Cart from "@components/Cart";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+    <Head>
+      <title>Troy Large</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="An ECommerce pastel artists platform for displaying, sharing & selling content"
+      />
+      <link rel="icon" type="image/jpg" href="/icon.ico" sizes="any" />
+    </Head>
       <body>
         <Header />
         <main className="mt-20">{children}</main>
